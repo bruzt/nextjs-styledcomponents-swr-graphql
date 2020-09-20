8714f73be462aed2ab7735669dfb780b06e10001
Bruno Zutim <bzutim@gmail.com>
2020-09-20 08:27:06 -0300

    add env variables

---
+++ next.config.js
@@ -0,0 +1,19 @@
+const withImages = require('next-images');
+const dotenv = require('dotenv');
+
+let env;
+if(process.env.NODE_ENV === 'production') env = '.env';
+else if(process.env.NODE_ENV === 'test') env = '.env.test';
+else env = '.env.dev';
+
+dotenv.config({
+    path: env
+});
+
+/////////////////////////////
+
+module.exports = withImages(/*{
+    env: {
+        BACKEND_URL: process.env.BACKEND_URL
+    }
+}*/);
--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ let token = null;
 if(process.browser) token = sessionStorage.getItem('token');
 
 export default axios.create({
-    baseURL: 'http://localhost:3001/graphql',
+    baseURL: `${process.env.BACKEND_URL}/graphql`,
     headers: {
         authorization: (token) ? `Bearer ${token}` : undefined
     }
